f11d50f25466689e03ddac4c9ac5972fa18c2332
ishwork79-source <ishwawork79@gmail.com>
2026-08-28 21:58:51 +0530

    Add Practical 3 stack implementation

---
+++ Stack/stack.c
@@ -0,0 +1,121 @@
+#include <stdio.h>
+
+#define MAX 100
+
+int stack[MAX];
+int top = -1;
+
+// Push operation
+void push()
+{
+    int value;
+
+    if (top == MAX - 1)
+    {
+        printf("Stack Overflow!\n");
+    }
+    else
+    {
+        printf("Enter the value to push: ");
+        scanf("%d", &value);
+
+        top++;
+        stack[top] = value;
+
+        printf("%d pushed into the stack.\n", value);
+    }
+}
+
+// Pop operation
+void pop()
+{
+    if (top == -1)
+    {
+        printf("Stack Underflow!\n");
+    }
+    else
+    {
+        printf("%d popped from the stack.\n", stack[top]);
+        top--;
+    }
+}
+
+// Peek operation
+void peek()
+{
+    if (top == -1)
+    {
+        printf("Stack is empty.\n");
+    }
+    else
+    {
+        printf("Top element is: %d\n", stack[top]);
+    }
+}
+
+// Display operation
+void display()
+{
+    int i;
+
+    if (top == -1)
+    {
+        printf("Stack is empty.\n");
+    }
+    else
+    {
+        printf("Stack elements are:\n");
+
+        for (i = top; i >= 0; i--)
+        {
+            printf("%d\n", stack[i]);
+        }
+    }
+}
+
+int main()
+{
+    int choice;
+
+    do
+    {
+        printf("\n===== STACK USING ARRAY =====\n");
+        printf("1. Push\n");
+        printf("2. Pop\n");
+        printf("3. Peek\n");
+        printf("4. Display\n");
+        printf("5. Exit\n");
+
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch (choice)
+        {
+            case 1:
+                push();
+                break;
+
+            case 2:
+                pop();
+                break;
+
+            case 3:
+                peek();
+                break;
+
+            case 4:
+                display();
+                break;
+
+            case 5:
+                printf("Exiting program...\n");
+                break;
+
+            default:
+                printf("Invalid choice! Please try again.\n");
+        }
+
+    } while (choice != 5);
+
+    return 0;
+}
