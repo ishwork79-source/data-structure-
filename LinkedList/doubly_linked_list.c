3d26f5585088c28a5c8c7ee399a9f80436679aed
ishwork79-source <ishwawork79@gmail.com>
2026-09-07 13:50:25 +0530

    Add implementation of doubly linked list with insertion and display functions

---
+++ LinkedList/doubly_linked_list.c
@@ -0,0 +1,185 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node *prev;
+    struct Node *next;
+};
+
+struct Node *head = NULL;
+
+// Insert at Beginning
+void insertBeginning()
+{
+    int value;
+
+    printf("Enter value: ");
+    scanf("%d", &value);
+
+    struct Node *newNode = (struct Node *)malloc(sizeof(struct Node));
+
+    newNode->data = value;
+    newNode->prev = NULL;
+    newNode->next = head;
+
+    if (head != NULL)
+    {
+        head->prev = newNode;
+    }
+
+    head = newNode;
+
+    printf("%d inserted at the beginning.\n", value);
+}
+
+// Insert at End
+void insertEnd()
+{
+    int value;
+
+    printf("Enter value: ");
+    scanf("%d", &value);
+
+    struct Node *newNode = (struct Node *)malloc(sizeof(struct Node));
+
+    newNode->data = value;
+    newNode->next = NULL;
+
+    if (head == NULL)
+    {
+        newNode->prev = NULL;
+        head = newNode;
+    }
+    else
+    {
+        struct Node *temp = head;
+
+        while (temp->next != NULL)
+        {
+            temp = temp->next;
+        }
+
+        temp->next = newNode;
+        newNode->prev = temp;
+    }
+
+    printf("%d inserted at the end.\n", value);
+}
+
+// Insert After Given Node
+void insertAfter()
+{
+    int value, afterValue;
+
+    if (head == NULL)
+    {
+        printf("Linked list is empty.\n");
+        return;
+    }
+
+    printf("Enter value to insert: ");
+    scanf("%d", &value);
+
+    printf("Enter the value after which to insert: ");
+    scanf("%d", &afterValue);
+
+    struct Node *temp = head;
+
+    while (temp != NULL && temp->data != afterValue)
+    {
+        temp = temp->next;
+    }
+
+    if (temp == NULL)
+    {
+        printf("Node with value %d not found.\n", afterValue);
+        return;
+    }
+
+    struct Node *newNode = (struct Node *)malloc(sizeof(struct Node));
+
+    newNode->data = value;
+
+    newNode->next = temp->next;
+    newNode->prev = temp;
+
+    if (temp->next != NULL)
+    {
+        temp->next->prev = newNode;
+    }
+
+    temp->next = newNode;
+
+    printf("%d inserted after %d.\n", value, afterValue);
+}
+
+// Display
+void display()
+{
+    struct Node *temp = head;
+
+    if (head == NULL)
+    {
+        printf("Linked list is empty.\n");
+        return;
+    }
+
+    printf("Doubly Linked List: ");
+
+    while (temp != NULL)
+    {
+        printf("%d <-> ", temp->data);
+        temp = temp->next;
+    }
+
+    printf("NULL\n");
+}
+
+int main()
+{
+    int choice;
+
+    do
+    {
+        printf("\n===== DOUBLY LINKED LIST =====\n");
+        printf("1. Insert at Beginning\n");
+        printf("2. Insert at End\n");
+        printf("3. Insert After Given Node\n");
+        printf("4. Display\n");
+        printf("5. Exit\n");
+
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch (choice)
+        {
+            case 1:
+                insertBeginning();
+                break;
+
+            case 2:
+                insertEnd();
+                break;
+
+            case 3:
+                insertAfter();
+                break;
+
+            case 4:
+                display();
+                break;
+
+            case 5:
+                printf("Exiting program...\n");
+                break;
+
+            default:
+                printf("Invalid choice! Please try again.\n");
+        }
+
+    } while (choice != 5);
+
+    return 0;
+}
